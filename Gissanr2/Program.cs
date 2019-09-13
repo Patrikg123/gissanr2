da12ae615365fd13970fc9102de754f7316e6c25
Patrik Gustafsson <patrikg123@hotmail.com>
2019-09-13 08:36:06 +0200

    Lets go

---
+++ Gissanr2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gissanr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Random r = new Random();
+            int rattsvar = r.Next(10) + 1;
+            int loop = 1;
+            while (loop == 1)
+            {
+                Console.WriteLine("Gissa Numret");
+                string textinstring = Console.ReadLine();
+                int textin = Int32.Parse(textinstring);
+                if (textin == rattsvar)
+                {
+                    Console.WriteLine("Rätt!");
+                    loop = 0;
+                }
+                if (textin < rattsvar)
+                    Console.WriteLine("Rätt svar är högre!");
+                if (textin > rattsvar)
+                    Console.WriteLine("Rätt svar är lägre!");
+
+            }
+        }
+    }
+}
+
